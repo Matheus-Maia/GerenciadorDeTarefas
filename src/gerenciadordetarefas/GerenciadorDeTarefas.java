27a85fe6236b59fcbcff6664d5941f50bdd66929
Matheus_Maia <matheusmaiadev535@gmail.com>
2025-04-24 16:47:13 -0300

    iniciao do projeto

---
+++ src/gerenciadordetarefas/GerenciadorDeTarefas.java
@@ -0,0 +1,16 @@
+
+package gerenciadordetarefas;
+
+/**
+ *
+ * @author Matheus Maia
+ */
+public class GerenciadorDeTarefas {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hellow! World...");
+        
+    }
+    
+}
